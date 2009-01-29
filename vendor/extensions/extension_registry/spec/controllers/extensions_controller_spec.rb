1ca479a89ecd8f7002298ddf4a1f49ca48d04dc1
steph <steph@ci1.endpoint.com>
2009-01-29 16:58:25 -0500

    Controller creation. Create a couple of base views for extension_registry.

---
+++ vendor/extensions/extension_registry/spec/controllers/extensions_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ExtensionsController do
+
+  #Delete this example and add some real ones
+  it "should use ExtensionsController" do
+    controller.should be_an_instance_of(ExtensionsController)
+  end
+
+end
